4e01c4426b2e161851f0da35cf7f8c1d19df7b7c
xdee <mail@x-dee.ru>
2019-11-05 14:43:04 +0500

    Extract some methods and cleanup

--- gphotosync.rb
+++ gphotosync.rb
@@ -5,6 +5,43 @@ require_relative 'options.rb'
 require_relative 'gplib.rb'
 require_relative 'storage.rb'
 
+# Top-level class doing all the logic
+class LibrarySync
+  def initialize(options)
+    @gphoto = GooglePhoto.new(options)
+    @storage = MediaStorage.new(options)
+    @options = options
+    @logger = Logger.new(@options.logfile, level: @options.loglevel)
+  end
+
+  def run
+    all_items = @gphoto.request_all_media{|items| sync items}
+    sync_deleted all_items
+  end
+
+  private
+
+  def sync(items)
+    items.each do |item|
+      next if @storage.exist? item
+      next if not_ready item
+      @storage.store item
+    end
+  end
+
+  def sync_deleted(all_items)
+    ids = all_items.map { |i| i[:id] }
+    items_to_delete = @storage.get_all.reject { |i| ids.include? i[:id] }
+    @logger.info "#{items_to_delete.length} item(s) going to be deleted"
+    items_to_delete.each { |item| @storage.remove item }
+  end
+
+  def not_ready(item)
+    item[:mimeType].start_with?('video') && item.dig(:mediaMetadata, :video, :status) != 'READY'
+  end
+end
+
+
 options = Options.new
 
 OptionParser.new do |opts|
@@ -32,36 +69,5 @@ OptionParser.new do |opts|
     options.logfile = filename
   end
 end.parse!
-
-class LibrarySync
-  def initialize(options)
-    @gphoto = GooglePhoto.new(options)
-    @storage = MediaStorage.new(options)
-    @options = options
-    @logger = Logger.new(@options.logfile, level: @options.loglevel)
-  end
-
-  def run
-    all_items = @gphoto.request_all_media do |items|
-      items.each do |item|
-        next if @storage.exist? item
-        next if not_ready item
-        @storage.store item
-      end
-    end
-
-    ids = all_items.map { |i| i[:id] }
-    items_to_delete = @storage.get_all.reject { |i| ids.include? i[:id] }
-    @logger.info "#{items_to_delete.length} item(s) going to be deleted"
-    items_to_delete.each { |item| @storage.remove item }
-
-  end
-
-  private
-
-  def not_ready(item)
-    item[:mimeType].start_with?('video') && item.dig(:mediaMetadata, :video, :status) != 'READY'
-  end
-end
 
 LibrarySync.new(options).run
