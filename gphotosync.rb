b49d6cefe1342d018b016fca3d48c2ac2efc0a64
xdee <mail@x-dee.ru>
2019-11-05 14:45:33 +0500

    extract LibrarySync  to separate file

--- gphotosync.rb
+++ gphotosync.rb
@@ -4,43 +4,7 @@ require 'optparse'
 require_relative 'options.rb'
 require_relative 'gplib.rb'
 require_relative 'storage.rb'
-
-# Top-level class doing all the logic
-class LibrarySync
-  def initialize(options)
-    @gphoto = GooglePhoto.new(options)
-    @storage = MediaStorage.new(options)
-    @options = options
-    @logger = Logger.new(@options.logfile, level: @options.loglevel)
-  end
-
-  def run
-    all_items = @gphoto.request_all_media{|items| sync items}
-    sync_deleted all_items
-  end
-
-  private
-
-  def sync(items)
-    items.each do |item|
-      next if @storage.exist? item
-      next if not_ready item
-      @storage.store item
-    end
-  end
-
-  def sync_deleted(all_items)
-    ids = all_items.map { |i| i[:id] }
-    items_to_delete = @storage.get_all.reject { |i| ids.include? i[:id] }
-    @logger.info "#{items_to_delete.length} item(s) going to be deleted"
-    items_to_delete.each { |item| @storage.remove item }
-  end
-
-  def not_ready(item)
-    item[:mimeType].start_with?('video') && item.dig(:mediaMetadata, :video, :status) != 'READY'
-  end
-end
-
+require_relative 'synclib.rb'
 
 options = Options.new
 
